85aec9dd69fdbfb6419d3d3a68d8dc0be45be86d
Samara Vitória <devsamaravitoria@gmail.com>
2023-11-29 00:26:08 -0300

    feat: Implementação do menor que e maior que

--- adivinhacao.py
+++ adivinhacao.py
@@ -5,10 +5,16 @@ print("********************************")
 numero_secreto = 43
 numero_user = input("Digite o seu número: ")
 numero_user_tratado = int(numero_user)
+acertou = numero_user_tratado == numero_secreto
+maior_que = numero_user_tratado > numero_secreto
+menor_que = numero_user_tratado < numero_secreto
 
 print("Você digitou ", numero_user)
 
-if numero_secreto == numero_user_tratado:
+if acertou :
     print("Você acertou!!")
 else:
-    print("Você errou =,)")
+    if(maior_que):
+        print("Você errou! O seu chute foi maior que o número secreto")
+    elif(menor_que):
+        print("Você errou! O seu chute foi menor que o número secreto")
