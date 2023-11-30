d31a657c7f41ef05e5b277ca44de2549904a88cb
Samara Vitória <devsamaravitoria@gmail.com>
2023-11-30 18:07:52 -0300

    refactor: melhora no processo de reiniciar o input

--- adivinhacao.py
+++ adivinhacao.py
@@ -3,18 +3,23 @@ print("Bem-vindo ao jogo de adivinhação")
 print("********************************")
 
 numero_secreto = 43
-numero_user = input("Digite o seu número: ")
-numero_user_tratado = int(numero_user)
-acertou = numero_user_tratado == numero_secreto
-maior_que = numero_user_tratado > numero_secreto
-menor_que = numero_user_tratado < numero_secreto
+total_tentativas = 3
 
-print("Você digitou ", numero_user)
+while (total_tentativas > 0):
+    numero_user = input("Digite o seu número: ")
+    numero_user_tratado = int(numero_user)
+    acertou = numero_user_tratado == numero_secreto
+    maior_que = numero_user_tratado > numero_secreto
+    menor_que = numero_user_tratado < numero_secreto
 
-if acertou :
-    print("Você acertou!!")
-else:
-    if(maior_que):
-        print("Você errou! O seu chute foi maior que o número secreto")
-    elif(menor_que):
-        print("Você errou! O seu chute foi menor que o número secreto")
+    print("Você digitou ", numero_user)
+
+    if acertou :
+        print("Você acertou!!")
+    else:
+        if(maior_que):
+            print("Você errou! O seu chute foi maior que o número secreto")
+        elif(menor_que):
+            print("Você errou! O seu chute foi menor que o número secreto")
+
+    total_tentativas = total_tentativas - 1
