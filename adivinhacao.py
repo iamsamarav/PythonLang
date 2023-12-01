f071d4ff07dda4c1ab23560819d961734e2d569c
Samara Vitória <devsamaravitoria@gmail.com>
2023-11-30 21:55:12 -0300

    feat: verificação de input

--- adivinhacao.py
+++ adivinhacao.py
@@ -6,22 +6,27 @@ numero_secreto = 43
 total_tentativas = 3
 rodada = 1
 
-while (rodada <= total_tentativas):
-    numero_user = input("Digite o seu número: ")
-    print(f"Tentativa {rodada} de {total_tentativas}".format(rodada, total_tentativas))
+for rodada in range(1,total_tentativas + 1):
+    numero_user = input("Digite o seu número entre e 100: ")
+    print("Tentativa {} de {}".format(rodada, total_tentativas))
     numero_user_tratado = int(numero_user)
+
+    if(numero_user_tratado < 1 or numero_user_tratado > 100):
+        print("Você deve digitar um número entre 1 e 100")
+        continue
     acertou = numero_user_tratado == numero_secreto
     maior_que = numero_user_tratado > numero_secreto
     menor_que = numero_user_tratado < numero_secreto
 
     print("Você digitou ", numero_user)
 
-    if acertou :
+    if acertou:
         print("Você acertou!!")
+        break
     else:
         if(maior_que):
             print("Você errou! O seu chute foi maior que o número secreto")
         elif(menor_que):
             print("Você errou! O seu chute foi menor que o número secreto")
 
-    rodada = rodada + 1
+print("Fim do jogo")
